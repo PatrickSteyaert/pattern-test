919c0ff3880c4f27ef29578a7d07bfe73a220358
PatrickSteyaert <patrick.steyaert@okaloa.com>
2021-11-13 06:25:31 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -3,7 +3,21 @@ import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>Pattern test</h1>`;
 
-type Pattern = RegExp | string;
+type SourcePattern = RegExp | string;
+type TargetPattern = string;
 
+function match(source: SourcePattern, target: TargetPattern): boolean {
+  if (source instanceof RegExp) {
+    return source.test(target);
+  } else if (typeof source === 'string') {
+    return source === target;
+  }
+}
+
+const sourceString: SourcePattern = 'sourceString';
+const matchAll: SourcePattern = /(.*?)/;
+
+console.log(match(sourceString, 'a'));
+console.log(match(matchAll, 'a'));
